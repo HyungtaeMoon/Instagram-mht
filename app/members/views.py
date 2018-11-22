28b25b187d3aee45082135f53188fdf0d30c2aa8
HyungtaeMoon <blessmht@gmail.com>
2018-11-22 21:59:35 +0900

    [ADD] signup view, url, template 연결

--- app/members/views.py
+++ app/members/views.py
@@ -1,8 +1,8 @@
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 
-from .forms import LoginForm
+from .forms import LoginForm, SignupForm
 
 
 def login_view(request):
@@ -15,12 +15,26 @@ def login_view(request):
             login(request, user)
             return redirect('posts:post-list')
 
-        else:
-            pass
-
     else:
         form = LoginForm()
         context = {
             'form': form,
         }
         return render(request, 'members/login.html', context)
+
+
+def logout_view(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('posts:post-list')
+
+
+def signup_view(request):
+    if request.method == 'POST':
+        pass
+    else:
+        form = SignupForm()
+        context = {
+            'form': form,
+        }
+        return render(request, 'members/signup.html', context)
