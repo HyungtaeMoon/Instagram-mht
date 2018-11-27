0106e0e2f8d647ee7a195430bdf7f9444c9463e4
HyungtaeMoon <blessmht@gmail.com>
2018-11-27 13:38:28 +0900

    [UPDATE] 유저 생성 로직을 SignupForm의 save 메서드로 구현

--- app/members/forms.py
+++ app/members/forms.py
@@ -1,5 +1,7 @@
 from django import forms
+from django.contrib.auth import login
 from django.contrib.auth.models import User
+from django.shortcuts import redirect
 
 
 class LoginForm(forms.Form):
@@ -60,3 +62,14 @@ class SignupForm(forms.Form):
             self.fields['password2'].widget.attrs['class'] += ' is-invalid'
             raise forms.ValidationError('비밀번호와 비밀번호 확인의 입력값이 다릅니다.')
         return password2
+
+    def save(self):
+        if self.errors:
+            raise ValueError('데이터 유효성 검증에 실패했습니다.')
+
+        else:
+            user = User.objects.create_user(
+                username=self.cleaned_data['username'],
+                password=self.cleaned_data['password1']
+            )
+            return user
