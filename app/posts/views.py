8fce8a099bd91398f6c3e016384285c9e3534201
HyungtaeMoon <blessmht@gmail.com>
2018-12-04 19:31:36 +0900

    [UPDATE] CommentCreateForm 사용

--- app/posts/views.py
+++ app/posts/views.py
@@ -1,16 +1,16 @@
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
 
 
 from .models import Post
-from .forms import PostCreateForm
+from .forms import PostCreateForm, CommentCreateForm
 
 
 def post_list(request):
     posts = Post.objects.all()
     context = {
         'posts': posts,
+        'comment_form': CommentCreateForm(),
     }
     return render(request, 'posts/post_list.html', context)
 
@@ -29,5 +29,13 @@ def post_create(request):
     return render(request, 'posts/post_create.html', context)
 
 
-def comment_create(request):
-    pass
+def comment_create(request, post_pk):
+    if request.method == 'POST':
+        post = Post.objects.get(pk=post_pk)
+        form = CommentCreateForm(request.POST)
+        if form.is_valid():
+            form.save(
+                post=post,
+                author=request.user,
+            )
+            return redirect('posts:post-list')
